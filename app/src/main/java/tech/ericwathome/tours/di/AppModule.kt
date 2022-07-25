86c77990a767f1fbf9a907f65d62a97e36828699
Eric Wafula <wathomeeric@gmail.com>
2022-07-25 12:25:07 +0300

    added hilt configuration

---
+++ app/src/main/java/tech/ericwathome/tours/di/AppModule.kt
@@ -0,0 +1,12 @@
+package tech.ericwathome.tours.di
+
+import android.app.Application
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
---
+++ app/src/main/java/tech/ericwathome/tours/di/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package tech.ericwathome.tours.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+}
