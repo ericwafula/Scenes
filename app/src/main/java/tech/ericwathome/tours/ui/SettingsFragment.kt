b6bc6b1440d0a8dd19f6c94b70d8ee1a291b17cd
ericwafula <ericwathome007@gmail.com>
2022-08-13 14:00:07 +0300

    added toolbar_menu.xml

--- app/src/main/java/tech/ericwathome/tours/ui/SettingsFragment.kt
+++ app/src/main/java/tech/ericwathome/tours/ui/SettingsFragment.kt
@@ -1,12 +1,24 @@
 package tech.ericwathome.tours.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import tech.ericwathome.tours.R
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        val nightModePref = findPreference<SwitchPreference>(getString(R.string.key_night_mode))
+        nightModePref?.setOnPreferenceChangeListener { preference, newValue ->
+            switchToNightMode(newValue as Boolean)
+            true
+        }
+    }
+
+    private fun switchToNightMode(newValue: Boolean?) {
+        Toast.makeText(requireContext(), "$newValue", Toast.LENGTH_LONG).show()
     }
 }
