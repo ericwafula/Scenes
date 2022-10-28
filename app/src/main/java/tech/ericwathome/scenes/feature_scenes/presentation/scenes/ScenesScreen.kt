66487305cfa7c71478370e8737cc8904f33690d4
ericwafula <ericwathome007@gmail.com>
2022-10-28 21:14:47 +0300

    added compose dependencies and updated ScenesFragment

---
+++ app/src/main/java/tech/ericwathome/scenes/feature_scenes/presentation/scenes/ScenesScreen.kt
@@ -0,0 +1,18 @@
+package tech.ericwathome.scenes.feature_scenes.presentation.scenes
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import tech.ericwathome.scenes.feature_scenes.presentation.ui.theme.ToursTheme
+
+@Composable
+fun ScenesScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        ToursTheme {
+
+        }
+    }
+}
